cd3fdf7d788878d4a5f1a9d16cd2c343b63f6abf
Yining Liu <m1030947133@163.com>
2024-04-28 13:42:00 -0700

    Update on utils.py
    move defined function into utlis

--- Streamlit/pages/1_From video script.py
+++ Streamlit/pages/1_From video script.py
@@ -1,6 +1,4 @@
 import streamlit as st
-import time
-import numpy as np
 import google.generativeai as genai
 from utils import logo,Title
 
@@ -11,27 +9,19 @@ st.set_page_config(page_title="Script", page_icon="📈")
 logo()
 Title()
 
-# 大标题
 st.markdown("# From video script")
-# 边栏标题
 st.write(
     """This page provides help you to create a successful youtube video from just an idea."""
 )
 
-# Define options for selection
 options = ["Simple idea", "Specific video script"]
-
-# Use radio or selectbox for selection
 selection = st.selectbox("Choose the way you want to express your idea:", options)
+
 idea = None
 target = None
 tone = None
 inputs = None
-
-# Get user input based on selection
-
 if selection == "Simple idea":
-  # Text input section
   idea = st.text_input("Enter your idea here:")
   target = st.text_input("Enter your target audience here:")
   tone = st.text_input("Enter your desired tone here:")
@@ -39,26 +29,19 @@ if selection == "Simple idea":
     inputs = "".join(['idea:',idea, 'target audience:', target, 'video tone:', tone])
   else:
     st.write("Please present your idea.")
-
-    
 elif selection == "Specific video script":
-  # Text file upload section
   uploaded_file = st.file_uploader("Upload your video script here", type="txt")
   if uploaded_file is not None:
     inputs = uploaded_file.read().decode("utf-8")
   else:
     st.write("Please upload your video script.")
-
-# Display user input
-
+    
 if inputs is not None:
-# Things I want Gemini to do
     promptTitle = ["Give me some attractive titles of youtube video for: ",inputs]
     promptDescription = ["Give me an example of youtube video description: ",inputs]
     promptThumbnail = ["Give me suggestions on how to make the thumbnail for this video idea attractive: ",inputs]
     promptTags = ["Give me some hastags I can use for this video of idea: ",inputs]
 
-    # Model response
     model = genai.GenerativeModel('gemini-pro')
     with st.spinner("Generating..."):
       responseTitle = model.generate_content(promptTitle,request_options={"timeout": 600})
@@ -66,7 +49,6 @@ if inputs is not None:
       responseThumbnail = model.generate_content(promptThumbnail,request_options={"timeout": 600})
       responseTags = model.generate_content(promptTags,request_options={"timeout": 600})
     
-
     tab1, tab2, tab3, tab4, tab5 = st.tabs([ "Example", "Titles", "Thumbnails", "Tags","Ex"])
     with tab1:
         st.write(responseDescription.text)
--- Streamlit/utils.py
+++ Streamlit/utils.py
@@ -25,3 +25,74 @@ def Title():
         """,
         unsafe_allow_html=True,
     )
+
+import os
+import shutil
+import tempfile
+import cv2
+
+# Constants
+FRAME_EXTRACTION_DIRECTORY = "./pages/content/frames"
+FRAME_PREFIX = "_frame"
+
+# Function to create or cleanup the frame extraction directory
+def create_frame_output_dir(output_dir):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    else:
+        shutil.rmtree(output_dir)
+        os.makedirs(output_dir)
+        
+# Function to extract frames from video (1 frame/second for demonstration)
+def extract_frame_from_video(upload_file):
+    create_frame_output_dir(FRAME_EXTRACTION_DIRECTORY)
+    
+    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+      temp_file.write(uploaded_file.read())
+      file_path = temp_file.name
+    #bytes_data = upload_file.read()
+    vidcap = cv2.VideoCapture(file_path)
+    fps = vidcap.get(cv2.CAP_PROP_FPS)
+    frame_duration = 1 / fps
+    output_file_prefix = os.path.basename(upload_file.name).replace('.', '_')
+    frame_count = 0
+    count = 0
+    while vidcap.isOpened():
+      success, frame = vidcap.read()
+      if not success:
+        break
+      if int(count / fps) == frame_count:
+        min = frame_count // 60
+        sec = frame_count % 60
+        time_string = f"{min:02d}:{sec:02d}"
+        image_name = f"{output_file_prefix}{FRAME_PREFIX}{time_string}.jpg"
+        output_filename = os.path.join(FRAME_EXTRACTION_DIRECTORY, image_name)
+        cv2.imwrite(output_filename, frame)
+        frame_count += 1
+      count += 1
+    vidcap.release()
+    print(f"Completed video frame extraction!\n\nExtracted: {frame_count} frames")
+    
+class File:
+      def __init__(self, file_path: str, display_name: str = None):
+        self.file_path = file_path
+        if display_name:
+          self.display_name = display_name
+        self.timestamp = get_timestamp(file_path)
+    
+      def set_file_response(self, response):
+        self.response = response
+
+def get_timestamp(filename):
+    parts = filename.split(FRAME_PREFIX)
+    if len(parts) != 2:
+        return None  # Indicates the filename might be incorrectly formatted
+    return parts[1].split('.')[0]
+
+# Make GenerateContent request with the structure described above.
+def make_request(prompt, files):
+    request = [prompt]
+    for file in files:
+        request.append(file.timestamp)
+        request.append(file.response)
+    return request
