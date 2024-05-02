acf7f2e4a2bbd16f02f08181df68abdcc48ee1cb
Yannis <13587224594@163.com>
2024-05-01 21:40:24 -0700

    Update 3_Video Analyzing 🎞️.py

--- Streamlit/pages/3_Video Analyzing 🎞️.py
+++ Streamlit/pages/3_Video Analyzing 🎞️.py
@@ -122,12 +122,12 @@ with tab5:
       st.write('Title: ',result['title'])
       st.write(f"Video URL: https://www.youtube.com/watch?v={result['id']}")
 
-if output['Title']:
-  DownloadButton = st.button('Download Feedback')
-  if DownloadButton:
-    file_data = generate_text_from_dict(output)
-    file_name = f'{uploaded_file.name}.txt'
-    download_file(file_data, file_name)
-  else:
-    st.info('Please press Analyze Button to get feedback.')
+# if output['Title']:
+#   DownloadButton = st.button('Download Feedback')
+#   if DownloadButton:
+#     file_data = generate_text_from_dict(output)
+#     file_name = f'{uploaded_file.name}.txt'
+#     download_file(file_data, file_name)
+#   else:
+#     st.info('Please press Analyze Button to get feedback.')
 
